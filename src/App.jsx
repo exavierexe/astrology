b00e7d9e4bcbd76360b697bf25935ae27daaf475
Exavier Lattimore <exavierexe@gmail.com>
2024-12-14 09:48:25 +1300

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
     <>
       <p>Exavier's School</p>
       <h1 class='text-xl font-bold'>Of Occult Sciences</h1>
+      <iframe data-tally-src="https://tally.so/embed/mVovO6?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="795" frameborder="0" marginheight="0" marginwidth="0" title="Birth Chart Reading"></iframe><script>var d=document,w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if("undefined"!=typeof Tally)v();else if(d.querySelector('script[src="'+w+'"]')==null){var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}</script>
+
     </>
   )
 }
